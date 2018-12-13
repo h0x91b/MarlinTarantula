c88703cb061aaf08dd4f5b0f5041d9cb5a18169b
Arseniy Pavlenko <h0x91b@gmail.com>
2018-12-13 12:13:02 +0200

    [+] Invert Y limit button + revert jerk anc accel settings to default from factory

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -920,9 +920,9 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
 #if ENABLED(DUAL_EXTRUDER)
-  #define DEFAULT_MAX_ACCELERATION      { 3000,  3000, 100, 10000, 10000 }
+  #define DEFAULT_MAX_ACCELERATION      { 2650,  2650, 100, 10000, 10000 }
 #else
-  #define DEFAULT_MAX_ACCELERATION      { 3000,  3000, 100, 10000 }
+  #define DEFAULT_MAX_ACCELERATION      { 2650,  2650, 100, 10000 }
 #endif
 
 /**
@@ -933,9 +933,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          2650    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  2650    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   2650    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -945,10 +945,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                  4.0
-#define DEFAULT_YJERK                  7.0
-#define DEFAULT_ZJERK                  0.2
-#define DEFAULT_EJERK                  2.5
+#define DEFAULT_XJERK                  15.0
+#define DEFAULT_YJERK                  15.0
+#define DEFAULT_ZJERK                  0.4
+#define DEFAULT_EJERK                  5.0
 
 /**
  * S-Curve Acceleration
